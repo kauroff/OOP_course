77c6d6e5cdca0d38ff3a64f0ad2d95484ccdfa14
Ivan Kaurov <crazyanimalcz@gamil.com>
2024-01-06 17:25:48 +0300

    Начатый код

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from abs import ABS
+from abc import ABC
 import os
 import requests
 
-class GetAPI(ABS):
-    def unit(self):
+class GetAPI(ABC):
+    def __init__(self, name, page, top_n):
+        self.name = name
+        self.page = page
+        self.top_n = top_n
+    def get_vacancies(self):
         pass
 
 
@@ -11,21 +15,26 @@ class GetAPI(ABS):
 
 
 class HeadHunterAPI(GetAPI):
-    def __init__(self):
+    def __init__(self, name, page, top_n):
+        super().__init__(name, page, top_n)
         self.url = 'https://api.hh.ru/'
-        self.params = {'count': 5, 'town': 'Moscow'}
 
-    def unit(self):
-        response =requests.get(self.url, headers={'User-Agent': 'MyApp/1.0 (crazyanimalcz@gmail.com)'})
-        return response
+
+    def get_vacancies(self):
+        """Выгрузка данных по 'HH' по запросам пользователя и возвращается словарь"""
+
+        data = requests.get(f'{self.url}/vacancies',
+                            params={'text': self.name, 'page': self.page, 'per_page': self.top_n}).json()
+        return data
+
 
 class SuperJobAPI(GetAPI):
-    def __init__(self):
+    def __init__(self, name, page, top_n):
+        super().__init__(name, page, top_n)
         self.url = 'https://api.superjob.ru/2.0/vacancies/'
-        # self.headers = {'X-Api-App-Id': os.getenv('SJ_API')}
         self.params = {'count': 5, 'town': 'Moscow'}
 
-    def unit(self):
+    def get_vacancies(self):
         response =requests.get(self.url, headers={'X-Api-App-Id': os.getenv('SJ_API')})
         data = response.json()
         return data
